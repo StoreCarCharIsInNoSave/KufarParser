0e5b710560e56c92b79c176222baf8c1c1cd36be
unknown <anonvtrede@gmail.com>
2021-12-15 16:04:10 +0300

    created WORKING console.log method (thx u expo)

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import {Image, ImageBackground} from "react-native";
 import {TouchableOpacity} from "react-native";
 import * as Progress from 'react-native-progress';
 import {TextInput} from "react-native";
 import {ScrollView} from "react-native";
-import  ScrollObject from './Components/ScrollObject';
+import ScrollObject from './Components/ScrollObject';
+import {parse} from 'node-html-parser';
+import axios from 'axios';
+import alert from "react-native-web/dist/exports/Alert";
+
 
 export default function App() {
 
@@ -14,7 +18,35 @@ export default function App() {
     const [dateTime, setDateTime] = React.useState('Отсутствует');
     const [findRequest, setFindRequest] = React.useState('');
 
+    //function split the string into an array of 10,000 characters
+    const Logger = (str) => {
+        let arr = [];
+        for (let i = 0; i < str.length; i += 9999) {
+            arr.push(str.substring(i, i + 9999));
+        }
+        arr.forEach((item, index) => {
+            setTimeout(() => {
+                console.log(item)
+            }, index * 100);
+        });
 
+    };
+
+
+    const qwe = () => {
+
+        axios.get('https://www.kufar.by/l/r~gomel?ot=1&query=%D0%B4%D0%B5%D0%BD%D1%8C%D0%B3%D0%B8').then(r => {
+            Logger(r.data.toString());
+        })
+
+    }
+
+
+    function RunParse() {
+
+        setDateTime(new Date().toLocaleString());
+        qwe();
+    }
 
     return (
         <View style={styles.content}>
@@ -30,7 +62,7 @@ export default function App() {
                         placeholderTextColor="grey"
                         onChangeText={(text) => setFindRequest(text)}/>
                     <TouchableOpacity style={styles.updateButton}
-                                      onPress={() => setDateTime(new Date().toLocaleString())}>
+                                      onPress={() => RunParse()}>
                         <Text style={styles.buttonText}>
                             Следить
                         </Text>
@@ -72,7 +104,7 @@ const styles = StyleSheet.create({
         borderWidth: 5,
         borderColor: 'grey',
     },
-    scrollObject:{
+    scrollObject: {
         fontSize: 20,
         color: 'black',
         textAlign: 'center',
